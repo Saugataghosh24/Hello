0134a5b0c0df7c3a0270051693aa18c696d31440
Saugataghosh24 <135034312+Saugataghosh24@users.noreply.github.com>
2023-06-05 10:11:22 +0530

    1st commit

---
+++ sum.java
@@ -0,0 +1,16 @@
+class Sum {
+	int a,b;
+	Sum(int a, int b){
+		this.a=a;
+		this.b=b;
+	}
+	void add(){
+		System.out.println(a+b);
+	}
+
+	public static void main(String args[]){
+	Sum s= new Sum(10,20);
+	s.add();
+}
+}
+
